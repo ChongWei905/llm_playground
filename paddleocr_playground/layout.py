eecd9ace38b53714b8a405bf858d2c408372da3c
Nemo <nemoweich@163.com>
2026-01-15 23:48:13 +0800

    chore: first commit

---
+++ paddleocr_playground/layout.py
@@ -0,0 +1,13 @@
+from paddleocr import PPStructureV3
+
+
+pipeline = PPStructureV3(
+    use_doc_orientation_classify=False,
+    use_doc_unwarping=False)
+output = pipeline.predict(
+    input="./pp_structure_v3_demo.png",
+)
+for res in output:
+    res.print()
+    res.save_to_json(save_path="output")
+    res.save_to_markdown(save_path="output")
